1891609e23a7171e274878aee77c6dd97bfe00af
Jorge Espinoza Chamaya <jorge.e2008@gmail.com>
2023-07-28 19:27:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() { 
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/member.html',
+        controller: 'MemberController',
+        controllerAs: 'memberCtrl'
+    };  
+    }
+
+
+
+
+
+
